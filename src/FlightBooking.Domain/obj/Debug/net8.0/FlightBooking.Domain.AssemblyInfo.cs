bdf8c55e3ecb297328783a8911d8c303d29d5ea6
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2026-04-07 23:34:39 -0300

    feat: add IFlightRepository interface
    - Created IFlightRepository to define flight data access contract
    - Established abstraction for persistence layer
    - Prepared domain for repository pattern implementation

--- src/FlightBooking.Domain/obj/Debug/net8.0/FlightBooking.Domain.AssemblyInfo.cs
+++ src/FlightBooking.Domain/obj/Debug/net8.0/FlightBooking.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlightBooking.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e2744c07556f27c845111d2edae1553648ddbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba50b86a87047a467de56720b3ef807aced1d5ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlightBooking.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlightBooking.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
